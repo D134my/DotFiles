223db2515b3780d250497945a292097d5a70268f
Ice <IceIsHere@gmail.com>
2021-12-14 11:59:30 +0330

    fix wallpaper problem

--- themeMe.sh
+++ themeMe.sh
@@ -2,7 +2,7 @@
 
 F=`awk '/file/ {print}' ~/.config/nitrogen/bg-saved.cfg | cut -c 6-100000`
 
-wal -i $F
+wal -i $F -e nitrogen --restore
 wal_i3rust -c ~/.config/i3status-rust/config.toml
 
 echo $F
