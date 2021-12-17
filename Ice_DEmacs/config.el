2a062e7d0cb6617401e73aad743c2f1c0a2653af
Ice <IceIsHere@gmail.com>
2021-12-17 18:24:17 +0330

    My Doom Emacs config for C/C++ development

--- Ice_DEmacs/config.el
+++ Ice_DEmacs/config.el
@@ -1,8 +1,6 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Ice"
       user-mail-address "IceIsHere@gmail.com")
